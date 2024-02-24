6515f16a1ebab00523e1b1e6163960e6c051e939
dungwoong <78326933+dungwoong@users.noreply.github.com>
2024-02-23 18:16:01 -0700

    register rope matrix as buffer

--- nodules/attention.py
+++ nodules/attention.py
@@ -17,6 +17,7 @@ class RoPESelfAttention(nn.Module):
         self.v = nn.Linear(hidden_size, hidden_size, bias=False)
 
         self.r = get_rotary_matrix(max_seq_len, hidden_size)
+        self.register_buffer('r', self.r)
         self.causal = causal
         if causal:
             self.register_buffer('tril', torch.tril(torch.ones(max_seq_len, max_seq_len)))
